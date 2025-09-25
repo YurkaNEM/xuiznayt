ec651066988fe387171f3ed72c2668a234bc74ce
YurkaNEM <yuratayants@gmail.com>
2025-09-25 21:18:46 +0300

    Delete lab1 directory

--- lab1/1.py
+++
@@ -1,3 +0,0 @@
-a = int(input("введите число: "))
-for i in range(1,a+1):
-    print(i)
--- lab1/2.py
+++
@@ -1,8 +0,0 @@
-a = int(input("введите число: "))
-b = int(input("введите число: "))
-if a > b:
-    print(a)
-elif a < b:
-    print (b)
-elif a == b:
-    print("числа одинаковы")
